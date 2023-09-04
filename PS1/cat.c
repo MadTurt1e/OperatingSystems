00b541df9c4e5f6abe6469f0afcbd01c61ba3e65
MadTurt1e <81546749+MadTurt1e@users.noreply.github.com>
2023-09-04 14:58:01 -0400

    modified:   PS1/cat 	modified:   PS1/cat.c

--- PS1/cat.c
+++ PS1/cat.c
@@ -1,17 +1,18 @@
 #include <errno.h> //error
 #include <fcntl.h> //const def
 #include <stdio.h> //tradition
+#include <string.h> //triviallizes strings
 
 
-int main(){
+//apparently this is supposed to be convention
+int main(int argc, char* argv[]){
+    char buffer[1024]; //buffer for the writing of characters
+    int rBytes, wBytes; //counter for number of bytes read and wrote
+    int iFile, oFile; //input and output file
 
-    //I/O recieved from the keyboard
-    char* input;
-    char* output;
-
-    //part 1: open the file we'd like to read / write
-    int inFile = open(input, O_RDONLY);
-
-    printf(inFile);
-    return 0;
+    //step 1: check if inputs are valid - the only invalid inputs are if there are no inputs, or if no output file is specified
+    if (argc == 0 || (argc == 1 && strcmp(argv[0], "-o"))){
+        printf("ERROR - NOT ENOUGH INPUTS SPECIFIED");
+        return -1;
+    }
 }
