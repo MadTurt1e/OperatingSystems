3fe9445fa49c3e446564b30d441497a829b0f830
Annie He <anniehe1126@gmail.com>
2023-09-22 23:37:35 -0400

    p3

--- PS2/ps2prob3/p3.c
+++ PS2/ps2prob3/p3.c
@@ -4,12 +4,13 @@
 #include <string.h>
 #include <errno.h>
 #include <dirent.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-void traverse(DIR*, char* ,FILE*);
+void traverse(char*, FILE*, struct stat*);
+bool compare(char*, FILE*, int);
 
 int main(int argc, char* argv[])  {
-    struct stat tmpStat;
-
     if (!argv[1]) {
         fprintf(stderr, "\e[0;31mError: No file name provided.\n\e[0m");
         exit(-1);
@@ -25,7 +26,7 @@ int main(int argc, char* argv[])  {
         fprintf(stderr, "\e[0;31mOpening file %s for read. Error: Not a regular file\n\e[0m", argv[1]);
         exit(-1);
     }
-    
+
     if (!argv[2]) {
         fprintf(stderr, "\e[0;31mError: no directory provided.\n\e[0m");
         exit(-1);
@@ -41,35 +42,80 @@ int main(int argc, char* argv[])  {
         fprintf(stderr, "\e[0;31mOpening directory %s for traversal. Error: Not a directory\n\e[0m", argv[2]);
         exit(-1);
     }
+    closedir(dir);
+
+    traverse(argv[2], fp, &fileStat);
 }
 
-void traverse(DIR* dir, char* dirName, FILE* fp) {
+void traverse(char* dirName, FILE* fp, struct stat* fileStat) {
+    DIR* dir = opendir(dirName);
     struct dirent* entry;
     while ((entry = readdir(dir)) != NULL) {
         char path[1024];
         snprintf(path, sizeof(path), "%s/%s", dirName, entry->d_name);
-        printf("%s\n", entry->d_name);
+        struct stat entryStat;
 
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
             continue;
-        }
-        struct stat info;
-        if (lstat(path, &info) < 0) {
-            fprintf(stderr, "\e[0;31mGetting stat of %s. Error: %s\n\e[0m", path, strerror(errno));
+        } else if (lstat(path, &entryStat) < 0) {
+            fprintf(stderr, "\e[0;33mGetting stat of %s. Warning: %s\n\e[0m", path, strerror(errno));
             continue;
-        }
-        if (S_ISREG(info.st_mode)) {
-            printf("%s is reg", path);
-            // compare files
-            // st_size equal?
-            // memcmp same?
-        } else if (S_ISLNK(info.st_mode)) {
-            printf("%s is lnk", path);
-            // look at link target
-        } else if (S_ISDIR(info.st_mode)) {
-            printf("%s is dir", path);
-            // recursive call
+        } else if (S_ISREG(entryStat.st_mode)) {
+            if (entryStat.st_ino == fileStat->st_ino) {
+                printf("%s HARD LINK TO TARGET\n", path);
+            } else if (entryStat.st_size == fileStat->st_size && compare(path, fp, entryStat.st_size)) {
+                printf("%s DUPLICATE OF TARGET (nlink=%d)\n", path, entryStat.st_nlink);
+            }
+        } else if (S_ISLNK(entryStat.st_mode)) {
+            char linkPath[1024];
+
+            ssize_t len = readlink(path, linkPath, sizeof(linkPath)-1);
+            if (len < 0) {
+                fprintf(stderr, "\e[0;33mReading symlink %s. Warning: %s\n\e[0m", path, strerror(errno));
+                continue;
+            }           
+            linkPath[len] = '\0';
+            struct stat linkStat;
+            if (stat(path, &linkStat) < 0) {
+                fprintf(stderr, "\e[0;33mGetting stat of %s. Warning: %s\n\e[0m", linkPath, strerror(errno));
+                continue;
+            } else if (!S_ISREG(linkStat.st_mode)) {
+                continue;
+            } else if (linkStat.st_ino == fileStat->st_ino) {
+                printf("%s SYMLINK (%s) RESOLVES TO TARGET\n", path, linkPath);
+            } else if (linkStat.st_size == fileStat->st_size && compare(path, fp, linkStat.st_size)) {
+                printf("%s SYMLINK (%s) RESOLVES TO DUPLICATE\n", path, linkPath);
+            }            
+        } else if (S_ISDIR(entryStat.st_mode)) {
+            traverse(path, fp, fileStat);
         }
     }
     closedir(dir);
+}
+
+bool compare(char* path, FILE* fp, int size) {
+    FILE* cmpFp = fopen(path, "r");
+
+    fseek(fp, 0, SEEK_SET);
+
+    char* buff1 = malloc(size);
+    char* buff2 = malloc(size);
+
+    if (!buff1 || !buff2) {
+        fprintf(stderr, "\e[0;33mAllocating buffer for %s. Warning: %s\n\e[0m", path, strerror(errno));
+        free(buff1);
+        free(buff2);
+        return false;
+    }
+
+    size_t bytesRead1 = fread(buff1, 1, size, cmpFp);
+    size_t bytesRead2 = fread(buff2, 1, size, fp);
+
+    int result = memcmp(buff1, buff2, size);
+    
+    free(buff1);
+    free(buff2);
+    fclose(cmpFp);
+
+    return result == 0;
 }
