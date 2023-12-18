b7fcec1bf6b29b08b0a0883fe2c39092ae098311
Annie He <anniehe1126@gmail.com>
2023-12-17 19:31:00 -0500

    updated files

--- PS7/prob3/nice.c
+++ PS7/prob3/nice.c
@@ -12,21 +12,21 @@
 pid_t* children;
 int numChild;
 
-// kill all files
+// kill all children
 void filicide(int signum) {
-    //kill all the children -pid number whenever we get the signal
+  //kill all the children -pid number whenever we wake up from alarm
   kill(-children[0], SIGKILL);
 
-  struct rusage usage = {0};
+  struct rusage usage;
   for (int i = 0; i < numChild; i++) {
-    // basically just wait for child to die now and print out a bit of the program info
+    // collect zombie children and print out their info
     wait4(children[i], NULL, 0, &usage);
     printf("Child %d: %ld.%06ld seconds\n", children[i], usage.ru_utime.tv_sec, usage.ru_utime.tv_usec);
   }
 }
 
 int main(int argc, char* argv[]) {
-    // quick arg check
+  // quick arg check
   if (argc != 4) {
     fprintf(stderr, "Error: wrong arguments\nUsage: ./nice [number of children] [niceness] [waittime]\n");
     exit(0);
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]) {
 
   children = malloc(numChild * sizeof(pid_t));
 
-  // signal handler for files
+  // signal handler to kill children
   signal(SIGALRM, filicide);
   alarm(atoi(argv[3]));
 
@@ -46,24 +46,24 @@ int main(int argc, char* argv[]) {
     pid_t pid = fork();
     switch (pid) {
       case -1: // oh no
-        fprintf(stderr, "Error: %s\n. Trying to fork.\n", strerror(errno));
+        fprintf(stderr, "Error: %s\nTrying to fork.\n", strerror(errno));
       case 0: //child case
         while(1);
       default: //parent case
-      //keep track of child pid
+        //keep track of child pid
         children[i] = pid;
-        // if we fail to set the priority just kill the entire thing
+        // most likely because we don't have permission
         if (i == 0) {
           if (setpriority(PRIO_PROCESS, pid, atoi(argv[2])) == -1) {
-            fprintf(stderr, "Error: %s\n. Trying to set niceness of process %d.\n", strerror(errno), pid);
+            fprintf(stderr, "Error: %s\nTrying to set niceness of process %d.\n", strerror(errno), pid);
             exit(0);
           }
         }
 
-        //set niceness of the child accordingly. 
+        //nice or naughty kid?
         setpgid(pid, children[0]);
     }
   }
-  //basically at this point we'd just wait for everything to finish up. 
+  //we sleep until argv[3] o'clock in the morning. 
   pause();
 }
